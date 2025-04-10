78af320d47e35b41e2c22c63c470cbb7be9f9e62
mehadi-shuvo <mehadihasanshuvo88@gmail.com>
2025-04-10 20:57:39 +0600

    feat: add mongodb connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
+import mongoose from "mongoose";
 import app from "./app";
 
 import { config } from "dotenv";
 config();
 
 const PORT = process.env.PORT || 3333;
+const DB_URL = process.env.DB_URL as string;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(DB_URL);
+    console.log("mongodb connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err: any) {
+    throw new Error(err.massage);
+  }
+}
+
+main();
